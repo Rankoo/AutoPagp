3d6c9f567e34536f94398c90219b201af0ff28a9
= <=>
2025-11-12 00:16:04 -0500

    login, se agrego el endpoint, falta implementar el jwt

---
+++ Codigo/AutoAlertDB/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using AutoAlertDB.Models;
+using AutoAlertDB.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoAlertDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IConfiguration _configuration;
+        public AuthController(IUserRepository userRepository, IConfiguration configuration)
+        { 
+            _userRepository = userRepository;
+            _configuration = configuration;
+        }
+
+        [HttpPost("logIn")]
+        public async Task<ActionResult> LogIn(LogIn logIn)
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ Codigo/AutoAlertDB/Models/LogIn.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoAlertDB.Models
+{
+    public class LogIn
+    {
+        [Required]
+        public required string UserName { get; set; } = string.Empty!;
+
+        [Required]
+        public required string Password { get; set; } = string.Empty!;
+    }
+}
